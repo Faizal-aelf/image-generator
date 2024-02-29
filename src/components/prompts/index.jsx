751dfbe7cba4c7dd53e5f3c7cfa3f83d59c5424c
Ahamed Faizal <faizal.a@aelf.io>
2024-02-29 12:44:18 +0800

    Update in the table format

--- src/components/prompts/index.jsx
+++ src/components/prompts/index.jsx
@@ -3,15 +3,16 @@ import {Box, TextField, Button} from '@mui/material';
 import axios from 'axios';
 import Loader from '../loader';
 import {GENERATE_IMAGE_API} from '../../utils/constants';
-import {mockData} from './data';
+import {mockData} from './data/data';
 import {convertTableIntoArray} from './utils';
 import ImageCard from './components/card';
+import ChangeImageBackground from './components/changeImageBackground';
 import './style.css';
 
 const PromptsSection = () => {
     const [message, setMessage] = useState("Create a low complexity sprite pixel art of a cat. The cat should have a simple design suitable for a retro video game, with minimal detail to keep it recognizable but not intricate. Make sure there's only one figure of cat and no other illustration. The background should be a single color that contrasts with the cat to make the sprite clearly visible. Keep the background clean without any decorations.");
     const [isLoading, setLoading] = useState(false);
-    const [generatedPrompts, setGeneratedPrompts] = useState([]);
+    const [generatedPrompts, setGeneratedPrompts] = useState('');
 
     const handleGeneratePrompts = async () => {
         setLoading(true);
@@ -28,10 +29,10 @@ const PromptsSection = () => {
                 }
             );
             console.log("response: ", response);
-            setGeneratedPrompts(convertTableIntoArray(response.data));
+            setGeneratedPrompts(response.data);
         } catch (error) {
             console.error('Error generating prompts:', error);
-            setGeneratedPrompts([]);
+            setGeneratedPrompts('');
         } finally {
             console.log("Finally: ");
             setLoading(false);
@@ -57,16 +58,19 @@ const PromptsSection = () => {
                 <Button variant="contained" onClick={handleGeneratePrompts}>Generate Prompts</Button>
             </Box>
         </Box>
-        {generatedPrompts && 
-        <ul className='image-list'>
-            {generatedPrompts.map((item, index) => (
-                <li key={index}>
-                    <ImageCard {...item}/>
-                </li>
-            ))}    
-        </ul>}
+        {generatedPrompts && <div dangerouslySetInnerHTML={{ __html: generatedPrompts }} />}
+        
     </Box>
   );
 };
 
 export default PromptsSection;
+
+/**
+ <ul className='image-list'>
+    {generatedPrompts.map((item, index) => (
+        <li key={index}>
+            <ImageCard {...item}/>
+        </li>
+    ))}    
+</ul>} */
